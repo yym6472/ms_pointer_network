051218621ddbf89ea7876de70a71491f96ecbbd2
yym6472 <yanyuanmeng@bupt.edu.cn>
2020-03-25 14:59:41 +0800

    Modified code for test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 from typing import Any, Union, Dict, Iterable, List, Optional, Tuple
 
 from allennlp.models.archival import load_archive
@@ -15,7 +16,18 @@ from allennlp.data import vocabulary
 def main(args):
     archive = load_archive(args.output_dir)
     predictor = Predictor.from_archive(archive=archive, predictor_name="ms_pointer")
-    print(predictor.predict("任天堂 switch 主机 全新 一代 游戏机 体感 家用 电视", "任天堂 Nintendo 游戏机"))
+
+    source1 = "任天堂 switch 主机 全新 一代 游戏机 体感 家用 电视"
+    source2 = "任天堂 Nintendo 游戏机"
+    output_dict = predictor.predict(source1, source2)
+    log_probs = output_dict["predicted_log_probs"]
+    predicted_tokens = output_dict["predicted_tokens"]
+    
+    print(f"Top {len(log_probs)} predictions:")
+    for log_prob, tokens in zip(log_probs, predicted_tokens):
+        prob = math.exp(log_prob)
+        sentence = " ".join(tokens)
+        print(f"    {prob:.2f}    {sentence}")
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
